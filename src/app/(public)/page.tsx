efa81f3377f59b8e9e535a09ecd9ddc1f842a61e
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2026-01-08 23:32:48 +0600

    popular products section with responsive

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,7 @@
 import Features from "@/components/module/Home/Features";
 import Hero from "@/components/module/Home/Hero";
 import PopularCategories from "@/components/module/Home/PopularCategories";
+import PopularProducts from "@/components/module/Home/PopularProducts";
 
 export default function page() {
     return (
@@ -8,6 +9,7 @@ export default function page() {
             <Hero />
             <Features />
             <PopularCategories/>
+            <PopularProducts/>
         </div>
     );
 }
